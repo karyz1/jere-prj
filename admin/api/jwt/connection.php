ac27c87c01ff796534a93c2969abef70b668b101
unknown <ukwajuabbakary@gmail.com>
2022-01-11 09:29:34 -0800

    fith

---
+++ admin/api/jwt/connection.php
@@ -0,0 +1,3 @@
+<?php
+
+$con=mysqli_connect('sql5.freemysqlhosting.net','sql5464974','tyk9FECuqr','sql5464974');
--- admin/api/jwt/meeting.php
+++ admin/api/jwt/meeting.php
@@ -1,6 +1,6 @@
 <?php
     include_once ('zoom_Api.php');
-    include ('connection.php');
+    include('connection.php');
 
 
     $error=0;
